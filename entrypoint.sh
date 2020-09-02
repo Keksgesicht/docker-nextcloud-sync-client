23a58c768a4230ab57f09f5d5634be42897b6a72
Jan B <keksgesicht23@gmail.com>
2020-09-02 16:11:48 +0200

    explicit root-user support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
-useradd --uid $UID --gid $GID -m nextcloud
-su nextcloud -c ${NC_DIR_BIN}/nc_sync.sh
+if [ -n "$UID" ] && [ -n "$GID" ] && [ "$UID" != "0" ] && [ "$GID" != "0" ]; then
+	useradd --uid $UID --gid $GID -m nextcloud
+	su nextcloud -c ${NC_DIR_BIN}/nc_sync.sh
+	exit 0
+fi
+${NC_DIR_BIN}/nc_sync.sh
